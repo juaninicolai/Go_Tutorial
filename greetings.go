0aff6247daf4649a1d43f59effccbee9a2cf2125
juaninicolai <juan0719@stud.kea.dk>
2022-07-22 19:08:56 +0200

    ready to start billing app

--- greetings.go
+++ greetings.go
@@ -1,13 +1,1 @@
 package main
-
-import "fmt"
-
-func sayHello(name string) {
-	fmt.Println("Hello,", name)
-}
-
-var points = []int{20, 90, 80, 50, 75}
-
-func showScore() {
-	fmt.Println("you scored this many points:", score)
-}
